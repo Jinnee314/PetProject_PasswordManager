bee8b563c24c76ae8ceb79b4487c6065d6c245a9
Vlad Tishkov <jinnes314@gmail.com>
2024-10-01 00:45:18 +0300

    Внёс изменения согласно новому видению.

--- PasswordManager.cpp
+++ PasswordManager.cpp
@@ -1,2 +1,22 @@
 #include "PasswordManager.hpp"
 #include <iostream>
+
+bool operator==(const Record& l, const Record& r)
+{
+	return tie(l.name, l.login, l.password, l.description) == tie(r.name, r.login, r.password, r.description);
+}
+
+bool operator!=(const Record& l, const Record& r)
+{
+	return tie(l.name, l.login, l.password, l.description) != tie(r.name, r.login, r.password, r.description);
+}
+
+std::ostream& operator<<(std::ostream& out, const Record& rec)
+{
+	out << "(" << rec.name << ", "
+		<< rec.login << ", "
+		<< rec.password << ", "
+		<< rec.description << ")";
+
+	return out;
+}
--- PasswordManager.hpp
+++ PasswordManager.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 #include <string>
 #include <filesystem>
 #include <map>
@@ -8,29 +9,40 @@ struct Record
 	std::string name, login, password, description;
 };
 
+bool operator==(const Record& l, const Record& r);
+
+bool operator!=(const Record& l, const Record& r);
+
+std::ostream& operator<<(std::ostream& out, const Record& rec);
+
 class PasswordManager
 {
 private:
-	std::filesystem::path wayToStorage;
+	std::filesystem::path wayToStorages;
 	std::map<std::string, Record> data;
 
 public:
-	PasswordManager(std::filesystem::path wayToStorage);
+	explicit PasswordManager(std::filesystem::path wayToStorage);
 
-	void AddRecord(
+	bool AddRecord(
 		const std::string& name,
 		const std::string& login,
 		const std::string& password,
 		const std::string& description = "");
+	bool AddRecord(const Record& rec);
+	//bool AddRecord(Record rec);
 
-	void showRecords();
+	std::vector<std::string> getNames() const;
+	std::map<std::string, Record> getRecords() const;
 
 	void deleteRecordByName(const std::string& name);
 	void deleteRecordByNumber(size_t number);
 
-	void getRecordByName(const std::string& name);
-	void getRecordByNumber(size_t number);
+	Record& getRecordByName(const std::string& name) const;
+	Record& getRecordByNumber(size_t number) const;
 
-	void getRecord(const std::string& input);	
+	Record& getRecord(const std::string& input) const;	
+
+	void clearData();
 };
 
