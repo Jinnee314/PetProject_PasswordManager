e8c3ecbe1bc467a0eb5dd6f4d0b7711d150e6be4
Vlad Tishkov <jinnes314@gmail.com>
2024-10-01 18:49:50 +0300

    Написал общую логику main и ряд объявлений
    Написал main так, как он представляется на первый взгляд.
    Объявил ряд функций, необходимых для main, а так же функции которые
    понадобятся объявленным функциям.
    Определил вспомогательные перечисления и псевдонимы.
    
    Объявленые функции:
    	Вспомогательные:
    	vector<string_view> splitWords(string_view str);
    	vector<string_view> splitLongStringForOut(string_view str);
    	CommandWithArgs parseCommandWithArgs(const string& str);
    
    	Необходимы main:
    	void showHelp();
    	void showNameSavedRecords(const PasswordManager& ps);
    	void showRecord(PasswordManager& ps, const vector<Arg>& args);
    	void addNewRecord(PasswordManager& ps, const vector<Arg>& args);
    	void deleteRecord(PasswordManager& ps, const vector<Arg>& args);
    
    Определённые перечисления:
    	enum class TypeCommand
    	enum class TypeArgs
    
    Определённые псевдонимы:
    	Arg = pair<TypeArgs, string>;
    	CommandWithArgs = pair<TypeCommand, vector<Arg>>;

---
+++ Main.cpp
@@ -0,0 +1,66 @@
+#include "PasswordManager.hpp"
+
+#include <iostream>
+#include <string>
+#include <iterator>
+#include <sstream>
+#include <iomanip>
+
+using namespace std;
+
+enum class TypeCommand{
+	Add, Delete, Show, Change, Help
+};
+
+enum class TypeArgs{
+	Name, Login, Password, Description, Number
+};
+
+using Arg = pair<TypeArgs, string>;
+using CommandWithArgs = pair<TypeCommand, vector<Arg>>;
+
+vector<string_view> splitWords(string_view str);
+vector<string_view> splitLongStringForOut(string_view str);
+CommandWithArgs parseCommandWithArgs(const string& str);
+
+void showHelp();
+void showNameSavedRecords(const PasswordManager& ps);
+void showRecord(PasswordManager& ps, const vector<Arg>& args);
+void addNewRecord(PasswordManager& ps, const vector<Arg>& args);
+void deleteRecord(PasswordManager& ps, const vector<Arg>& args);
+
+int main()
+{
+	PasswordManager ps("testStorage");
+
+	showNameSavedRecords(ps);
+
+	cout << "\n" << "Write help to get information about the commands.\nEnter command:\n";
+
+	string commandWithArgs;
+	while (getline(cin, commandWithArgs))
+	{
+		auto [comm, args] = parseCommandWithArgs(commandWithArgs);
+
+		switch (comm)
+		{
+		case TypeCommand::Help:
+			showHelp();
+			break;
+		case TypeCommand::Add:
+			addNewRecord(ps, args);
+			break;
+		case TypeCommand::Delete:
+			deleteRecord(ps, args);
+			break;
+		case TypeCommand::Show:
+			showRecord(ps, args);
+			break;
+		default:
+			break;
+		}
+	}
+
+	return 0;
+}
+
