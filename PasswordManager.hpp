45dbccb475ddb7024effd7047c6122aeba03957f
Vlad Tishkov <jinnes314@gmail.com>
2024-10-07 22:43:21 +0300

    Добавил комментарии в PasswordManager.hpp

--- PasswordManager.hpp
+++ PasswordManager.hpp
@@ -13,10 +13,15 @@ struct Record
 	std::string name, login, password, description;
 };
 
-bool operator==(const Record& l, const Record& r);
 
+// Эти перегрузки необходимы для корректной работы 
+// юнит тестов.
+bool operator==(const Record& l, const Record& r);
 bool operator!=(const Record& l, const Record& r);
 
+// Перегрузка оператора вывода для структуры Record.
+// Выводит только имя и описание. Описание не шире 
+// WIDTH_OUT_STRING (из файла parser.hpp).
 std::ostream& operator<<(std::ostream& out, const Record& rec);
 
 class PasswordManager
@@ -25,53 +30,135 @@ private:
 	using DataStorage = std::map<std::string, Record>;
 	using DataIter = DataStorage::iterator;
 	
+	// ПУть до файла-хранилище. В данный файл запишутся все данные после окончания
+	// работы с менеджером. Нужна для удобства и дальнейшего расширения программы.
 	std::filesystem::path fileStorage;
+
+	// Хранилище записей. Ключом выступает копия имени записи.
 	DataStorage data;
+	
+	// Текущая запись. Последняя запись, которую искали методами 
+	// getRecordByName, getRecordByNumber. Используется для быстрой 
+	// работы с интересующей нас записью.
 	DataIter currRec;
 
+	// Строка, в которую записываются данные из файла и из которой
+	// данные записываются в файл.
 	std::string fileData;
 
+	// ключ шифрования и вектор инициализации
 	std::array<unsigned char, CryptoPP::AES::MAX_KEYLENGTH> aesKey;
 	std::array<unsigned char, CryptoPP::AES::BLOCKSIZE> cbcIv;
 
 	// Получение итератора по номеру.
-	// Проверка номера на выход за пределы размера 
-	// возлагаются на вызывающую функцию.
+	// Проверка номера на выход за пределы размера возлагаются на вызывающую функцию.
 	DataIter getIterByNumber(size_t number);
 
+	// Вычисляет из мастер ключа хэш значение с помощью SHA384 и из него получает
+	// ключ шифрования (первые 256 бит хэша) и вектор инициализации (оставшиеся 128 бит хэша)
 	void createKeyAndIv(std::string masterKey);
+
+	// Из строки fileData заполняет словарь записей data.
+	// В строке записи разделены символов ';', а поля одной записи разделены пробелами.
 	void createStringFromData();
+
+	// Из словаря data создаёт строку и сохраняет результат в fileData.
+	// В строке записи разделены символов ';', а поля одной записи разделены пробелами.
 	void createDataFromString();
+
+	// Записывает строку fileData в файл fileStorage, открытый в бинарном режиме.
 	void writeDataInFile();
 
+
+	// Расшифровывает/зашифровывает данные из строки fileData с помощью AES256
+	// и записывает результат в неё же.
 	void decrypt();
 	void encrypt();
 
 public:
 	PasswordManager() = default;
+
+	// В деструкторе зашифровываем данные и записываем их в файл fileStorage.
+	// Если data пусто из-за того, что пользователь удалил все данные, очищает файл
+	// от старых записей.
+	// Если data пусто из-за того, что не был введён правильный мастер ключ, то ничего
+	// не делает.
 	~PasswordManager();
 
+	// Считывание данных из файла. Отдельно и в публичной секции 
+	// из-за того, что считывание файла должно происходить параллельно
+	// тому, как пользователь вводит мастер пароль.
+	// Считывает данные в строку fileData.
+	// Сохраняет путь до файла в storageFile.
+	// Если файл пуст, то только сохраняет путь до файла в storageFile.
 	void readDataFromFile(std::filesystem::path file);
+
+	// Вычисляет ключ шифрования и вектор инициализации. Расшифровывает данные, 
+	// полученные из файла, и формирует из  них словарь записей data. Присваивает 
+	// currRec значение data.end()
+	// Если строка fileData пуста, то только вычисляет ключ шифрования и вектор инициализации.
 	void decryptData(std::string masterKey);
 
+
+	 /*
+		Методы добавления записи.
+	 */
+	
+	// Исторический метод, с ним писались тесты, в самой программе не
+	// используется. Создаёт запись из отдельных строк и вызывает вторую
+	// функцию добавления записи.
 	void addRecord(
 		std::string name,
 		std::string login,
 		std::string password,
 		std::string description = "");
+
+	// Добавление записи в словарь data.
 	void addRecord(Record rec);
 
+
+	/*
+		Методы удаления записей.	
+	*/
+	
+	// Удаляет запись по имени.
+	// Если записи с таким имени нет, ничего не делает.
 	void deleteRecordByName(std::string name);
+
+	// Удаляет запись по номеру.
+	// Если записи с таким номер нет, ничего не делает
 	void deleteRecordByNumber(size_t number);
+
+	// Удаляет запись из currRec.
+	// После удаления currRec = data.end()
 	void deleteCurrent();
 
+
+	/*
+		Методы изменения полей текущей записи.
+	*/
+
+	// При изменении имени запись со старым именем заменяется
+	// записью с новым именем
 	void changeNameRecord(std::string newName);
 	void changeLoginRecord(std::string newLogin);
 	void changePasswordRecord(std::string newPass);
 	void changeDescriptionRecord(std::string newDes);
 
+
+	/*
+		Методы получения информации.
+	*/
+
+	// Возвращает все имена записей из data.
 	std::vector<std::string_view> getNames() const;
+
+	// Получение записи по имени. Если записи с таким нет, возвращает
+	// пустую запись.
 	Record getRecordByName(std::string name);
+
+	// Получение записи по номеру. Если записи под таким номером нет, 
+	// возвращает пустую запись.
 	Record getRecordByNumber(size_t number);
 	
 	std::string getCurrentRecordName();
@@ -79,8 +166,10 @@ public:
 	std::string getCurrentRecordPassword();
 	std::string getCurrentRecordDescription();
 
+
+	// Метод получения текущего количества записей
 	size_t numRecords() const;
 
+	// Метод очищает словарь data.
 	void clearData();
 };
-
